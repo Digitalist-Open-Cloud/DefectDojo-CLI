8171b52eeefcbd99eec846b6c3f7aae4f8424ffc
Mikke Schirén <mikke.schiren@digitalist.com>
2026-03-11 15:13:03 +0100

    add build change

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM python:3.13.3-alpine3.21
+FROM python:3.14.0-alpine3.21
 ARG VERSION="0.1.18"
-
 RUN pip install --no-cache-dir --upgrade pip  && \
     pip install --no-cache-dir defectdojo-cli2==${VERSION}
-
 CMD ["/usr/local/bin/defectdojo"]
--- release.sh
+++ release.sh
@@ -12,10 +12,14 @@ poetry install
 # Build
 poetry build
 
-# Add pblush token (only needed once)
+# Add publish token (only needed once)
 #poetry config pypi-token.pypi <your-token>
 
 poetry publish
+
+trivy filesystem . -f cyclonedx > sbom.json
+git add sbom.json
+git commit -m "updated SBOM for $version"
 
 # Commit ChangeLog and tag
 git tag -a "$version" -m "Release $version"
@@ -26,5 +30,7 @@ git push origin main --follow-tags
 sleep 60
 docker build --build-arg VERSION=${version} . -t digitalist/defectdojo-cli2:$version --platform=linux/amd64 --no-cache
 docker tag digitalist/defectdojo-cli2:$version digitalist/defectdojo-cli2:latest
+docker tag digitalist/defectdojo-cli2:${version} hub.dglive.net/public/defectdojo-cli2:${version}
 docker push digitalist/defectdojo-cli2:$version
 docker push digitalist/defectdojo-cli2:latest
+docker push hub.dglive.net/public/defectdojo-cli2:${version}
